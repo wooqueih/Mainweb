ac79216c2acf66402e251610553505e08b93c934
Michael Pogrebnjak <astroqueih@gmail.com>
2020-11-15 07:06:50 +0100

    2.1

--- hub/hub.js
+++ hub/hub.js
@@ -1,16 +1,17 @@
-/*function toDo() {
-    var lis = document.getElementById("toDo").innerHTML;
-    
-}*/
-function reqListener () {
-    console.log(this.responseText);
+function reqJSON() {
+    xmlHttpReq.addEventListener("load", reqListener)
+    xmlHttpReq.open("GET", "http://localhost:8080/to-do.json");
+    xmlHttpReq.send();
 }
 
+function reqListener () {
+    console.log(this.responseText);
+    var json = JSON.parse(xmlHttpReq.responseText);
+    console.log(json)
+    let str = json.wantToDo.toString();
+    wantList.innerHTML = str.split(",").join("<br>");
+}
+
+var wantList = document.getElementById("wantToDo");
+var haveList = document.getElementById("haveToDo");
 var xmlHttpReq = new XMLHttpRequest();
-xmlHttpReq.addEventListener("load", reqListener)
-xmlHttpReq.open("GET", "http://localhost:8080/to-do.json");
-xmlHttpReq.send();
-
-/*var json = JSON.parse(xmlHttpReq.responseText);
-console.log(json);*/
-
--- hubWebserver.js
+++ hubWebserver.js
@@ -2,20 +2,6 @@ var http = require("http");
 var fs = require("fs");
 var url = require('url');
 
-/*http.createServer(function (req, res) {
-    let q = url.parse(req.url, true);
-    let dat = "." + q.pathname;
-    fs.readFile(dat, function(err, data) {
-        if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end("404 Not Found");
-        }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
-}).listen(8080);
-*/
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
     if (q.pathname == "/") {
