0d006dc3ffedb88cdc9ad694e16a21cd540644b9
Michael Pogrebnjak <astroqueih@gmail.com>
2020-11-21 07:06:02 +0100

    3.3 dataVis still not working

--- dataVis/dataVis.js
+++ dataVis/dataVis.js
@@ -5,8 +5,18 @@ for(let i = 0; i < cvClass.length; i++){
 } var coordSys = {
     cv : undefined,
     canvasSelector : function(num){cv = cvClass.item(num);ctx = allCtx[num];},
-    numLine : function(xmax, xmin, ymax, ymin, linex, liney){
+    numLine : function(xmax, xmin, ymax, ymin){
         y = Math.abs(ymax) + Math.abs(ymin);
         x = Math.abs(xmax) + Math.abs(xmin);
+        ctx.strokeStyle = "black";
+        ctx.beginPath();
+        ctx.moveTo(0, y/2);
+        ctx.lineTo(cv.width, y/2);
+        ctx.moveTo(x/2, 0);
+        ctx.lineTo(x/2, cv.height);
+        ctx.stroke();
+        ctx.closePath();
     }
 };
+coordSys.canvasSelector(0);
+coordSys.numLine();
